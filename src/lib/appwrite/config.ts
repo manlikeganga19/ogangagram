4903c04e8dffb49c46fb2a237018dabb69c04f97
Daniel <owenjodaniel19@gmail.com>
2023-12-08 19:06:19 +0300

    auth functionalities

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,7 @@
+import {Client ,  Account, Databases , Storage, Avatars} from "appwrite"
+import { Import } from "lucide-react"
+
+
+export const appwriteConfig = {
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+}
