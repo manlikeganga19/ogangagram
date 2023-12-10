b89d7893b0a59cdbe2065c795b09b00ddbd1f1da
Daniel <owenjodaniel19@gmail.com>
2023-12-10 19:41:00 +0300

    appwrite bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { Toaster } from "@/components/ui/toaster"
 import "./globals.css";
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
+import SigninForm from '@/_auth/forms/SigninForm';
+import SignupForm from '@/_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
-import { Home } from './_root/pages';
+import { Home } from '@/_root/pages';
 
 const App = () => {
     return (
